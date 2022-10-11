6043ffe0bd540a2ca0ef4ca63d4c0b8d01818815
mostafizur610 <mostafizurrahman273@gmail.com>
2022-10-11 17:15:03 +0600

    added header section

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,26 @@
-import React from 'react';
+import Card from 'react-bootstrap/Card';
+import ispring from '../../images/ispring.png';
+import './Header.css';
 
-const Header = () => {
+function Header() {
     return (
-        <div>
-            <h2>This is header</h2>
-        </div>
+        <Card className="bg-dark text-warning header">
+            <Card.Img src={ispring} className='image' alt="" />
+            <Card.ImgOverlay className='mt-5'>
+                <Card.Title className='text-center'>More Than Just A Quiz Maker</Card.Title>
+                <Card.Text className='text-center'>
+                    iSpring is built to gamify your classroom for an interactive learning process
+                </Card.Text>
+            </Card.ImgOverlay>
+        </Card>
     );
-};
+}
 
 export default Header;
+
+
+
+
+
+
+
