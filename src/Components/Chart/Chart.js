2cad2fb19cf5ca2c96a4fae58e9cc4ebb994206b
mostafizur610 <mostafizurrahman273@gmail.com>
2022-10-12 10:43:17 +0600

    chart added

--- src/Components/Chart/Chart.js
+++ src/Components/Chart/Chart.js
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+
 
 const Chart = () => {
+    const [chart, setChart] = useState([]);
+    useEffect(() => {
+        fetch('https://openapi.programming-hero.com/api/quiz')
+            .then(res => res.json())
+            .then(data => {
+                const chart = data.data;
+                setChart(chart);
+            })
+    }, [])
+
     return (
-        <div>
-            <h2>Chart</h2>
+        // <ResponsiveContainer width="100%" height="100%">
+        // </ResponsiveContainer>
+        <div className='d-flex justify-content-center mt-5'>
+            <BarChart width={600} height={400} data={chart}>
+                <Bar dataKey="total" fill="#8884d8" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip></Tooltip>
+            </BarChart>
         </div>
+
     );
 };
 
